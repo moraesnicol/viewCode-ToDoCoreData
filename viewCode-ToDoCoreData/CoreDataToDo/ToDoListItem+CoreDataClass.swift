476f641038ac2c2d5b78be5e2d9e48faa3bce7b2
Gabriel <moraesnicol@gmail.com>
2021-10-11 07:27:03 -0300

    Initial Commit

---
+++ viewCode-ToDoCoreData/CoreDataToDo/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  viewCode-ToDoCoreData
+//
+//  Created by Gabriel on 08/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
